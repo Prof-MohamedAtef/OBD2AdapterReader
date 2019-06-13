9218c07e685deff3c91f6082a48b02fcc24649d3
Prof-MohamedAtef <proofesser@gmail.com>
2019-06-13 12:13:45 -0700

    OBD2 protocol initialization app

---
+++ app/src/main/java/reader/adapter/android/obd2/prof/obdtwoandroidapp/MainActivity.java
@@ -0,0 +1,13 @@
+package reader.adapter.android.obd2.prof.obdtwoandroidapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
